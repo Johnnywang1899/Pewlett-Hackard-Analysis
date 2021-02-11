7e47da671f6e88163065795da59a8ab72c73f4bf
Yining (Johnny) Wang <yining.wang@mail.utoronto.ca>
2021-02-11 17:33:57 -0500

    new code and pics added

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -3,6 +3,8 @@ select * from titles
 select * from retirement_titles
 select * from unique_titles
 select * from dept_emp
+select * from departments
+select * from mentorship_eligibility
 
 SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
 INTO retirement_titles
@@ -37,3 +39,26 @@ ON e.emp_no = t.emp_no
 WHERE (de.to_date = '9999-01-01') 
 AND(e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
+
+SELECT COUNT(emp_no) from mentorship_eligibility
+SELECT COUNT(emp_no) from unique_titles
+
+-- Find the department name by sorting the number of retiring employees.
+SELECT COUNT(u.emp_no), d.dept_name
+FROM unique_titles as u
+JOIN dept_emp as de
+ON u.emp_no = de.emp_no
+JOIN departments as d
+ON de.dept_no = d.dept_no
+GROUP BY d.dept_name
+ORDER BY COUNT(u.emp_no) DESC;
+
+-- Find the department name by sorting the number of employees qualified for mentorship program
+SELECT COUNT (me.emp_no), d.dept_name
+FROM mentorship_eligibility as me
+JOIN dept_emp as de
+ON me.emp_no = de.emp_no
+JOIN departments as d
+ON de.dept_no = d.dept_no
+GROUP BY d.dept_name
+ORDER BY COUNT(me.emp_no) DESC;
